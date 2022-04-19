f86de685dd1c56bd4c49b4c0b0c1f8a779128e0d
Queena Tagala <prot3ch1993@gmail.com>
2022-04-19 23:21:29 +0800

    footer2

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -1,14 +1,33 @@
 import React from "react"
 
 const Footer = () =>{
-    const name = ""
     return (
-        <footer>
-            <p>
-                copyright {name}
-            </p>
-        </footer>
+        <div className="row">
+            <h1 className="logo">watchr</h1>
+        <div className="backgroundGradient row">
+            <div className="col-3">
+            <h1 className="logo">watchr</h1>
+            </div>
+            <div className="col-3">
+                <p className="footerHeading">Contact Us</p>
+                <p className="footerContent">adriisabz@gmail.com</p>
+                <p className="footerContent">09973950611</p>
+                <img src=""></img>
+                <img src=""></img>
+            </div>
+            <div className="col-3">
+            <p className="footerHeading">Terms Conditions</p>
+            <p className="footerContent"><a data-bs-toggle="collapse" href="#collapse1" role="button" aria-expanded="false" aria-controls="collapse1">Privacy Policy</a></p>
+            <p className='footerContent'><a data-bs-toggle="collapse" href="#collapse2" role="button" aria-expanded="false" aria-controls="collapse2">Refund and Cancellation Policy</a></p>
+            <p className='footerContent'><a data-bs-toggle="collapse" href="#collapse3" role="button" aria-expanded="false" aria-controls="collapse3">Terms of Service</a></p>
+            </div>
+            <div className="col-12">
+                <p className="footerContent">copyright 2022. All rights reserved</p>
+            </div>
+        </div>
+        </div>
     )
 }
 
+  
 export default Footer
