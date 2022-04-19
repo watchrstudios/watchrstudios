8fc7006915014147a64b4e57435d37c9c79db9cf
Queena Tagala <prot3ch1993@gmail.com>
2022-04-19 09:06:33 +0800

    Footer
    First commit

---
+++ src/Components/Footer.js
@@ -0,0 +1,14 @@
+import React from "react"
+
+const Footer = () =>{
+    const name = ""
+    return (
+        <footer>
+            <p>
+                copyright {name}
+            </p>
+        </footer>
+    )
+}
+
+export default Footer
--- src/index.js
+++ src/index.js
@@ -2,12 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import Hero from './Components/Hero';
-
+import Footer from './Components/Footer';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
  <>
   <Hero/>
+  <Footer/>
  </>
 );
 
