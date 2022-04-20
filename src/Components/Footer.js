225c6b30db039c049398fd9aee3ffc93e36548ff
Queena Tagala <prot3ch1993@gmail.com>
2022-04-20 11:59:07 +0800

    2nd edit:footer

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -5,17 +5,17 @@ const Footer = () =>{
         <div className="row">
             <h1 className="logo col-12">watchr</h1>
         <div className="backgroundGradient row">
-            <div className="col-3">
+            <div className="col-md-3 col-12">
             <h1 className="logo">watchr</h1>
             </div>
-            <div className="col-3 col-s-12">
+            <div className="col-lg-3 col-12">
                 <p className="footerHeading">Contact Us</p>
                 <p className="footerContent">adriisabz@gmail.com</p>
                 <p className="footerContent">09973950611</p>
                 <img src=""></img>
                 <img src=""></img>
             </div>
-            <div className="col-3">
+            <div className="col-lg-3 col-12">
             <p className="footerHeading">Terms Conditions</p>
             <p className="footerContent"><a data-bs-toggle="collapse" href="#collapse1" role="button" aria-expanded="false" aria-controls="collapse1">Privacy Policy</a></p>
             <p className='footerContent'><a data-bs-toggle="collapse" href="#collapse2" role="button" aria-expanded="false" aria-controls="collapse2">Refund and Cancellation Policy</a></p>
